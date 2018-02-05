0943bedd71a9323638b966d1079238c3c2b48796
Joschua Fink <joschua.fink@gmx.de>
2018-02-05 20:37:30 +0100

    add pp2_12

--- bitwise_operations/pp2_12.c
+++ bitwise_operations/pp2_12.c
@@ -5,6 +5,16 @@ int least_significant( int x){
 	return x & 0xFF;
 }
 
+int complement_firstbytes ( int x){
+
+	return ((~x) & 0xFFFFFF00) | least_significant(x); 
+}
+
+int leastbytesone ( int x){
+
+	return (x | 0x000000FF); 
+}
+
 int main(int argc, char *argv){
 
 	int x = 0x87654321;
@@ -12,4 +22,9 @@ int main(int argc, char *argv){
 	printf("x = 0x%x\n", x);
 	
 	printf("Least Significant Byte: 0x%x\n", least_significant(x));	
+	
+	printf("Least Significant Byte: 0x%x\n", complement_firstbytes(x));	
+
+	
+	printf("Least Significant Byte: 0x%x\n", leastbytesone(x));	
 }
