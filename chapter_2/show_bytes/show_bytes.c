d012cdf188ff7e5bf87dbf9af6c1c7f9d2095880
Joschua Fink <fink@math.uni-frankfurt.de>
2018-02-09 13:51:18 +0100

    add is_little_endian.c

--- chapter_2/show_bytes/show_bytes.c
+++ chapter_2/show_bytes/show_bytes.c
@@ -33,7 +33,7 @@ void show_long(long x) {
 	show_bytes((byte_pointer) &x, sizeof(long));
 }
 
-void show_long(double  x) {
+void show_double(double x) {
 
 	show_bytes((byte_pointer) &x, sizeof(double));
 }
@@ -54,10 +54,36 @@ void test_show_bytes(int val) {
 	show_pointer(pval);
 }
 
+int keep_least_significant(int x, int y){
+
+	int ymask = 0xFFFFFF00;
+	int xmask = ~ymask;
+	int newx = x & xmask;
+	int newy = y & ymask;
+	return newx | newy;
+}
+
+void is_little_endian(){
+	
+	int ival = 1;
+	unsigned char *pointer = (unsigned char *) &ival;
+	if( *pointer == 1) printf("Little Endian\n");
+	else printf("Big Endian\n");
+		
+}
+
 int main() { 
 	const char *s = "abcdef";
-	int *i = 1;
+	int i = 16;
 	show_bytes((byte_pointer) s, strlen(s));
 	show_int(i);
+	test_show_bytes(1);
+	is_little_endian();
+	
+	int x = 0x89ABCDEF;
+	int y = 0x76543210;
+	
+	printf("x = 0x%x\n y = 0x%x\n", x, y);
+	printf("keep_least_significant(x,y) = 0x%x\n", keep_least_significant(x,y));
 	return 0;
 }
